b4073e2bdb6ed07cdb706cf1447e32e39bf240d0
Dedmustdie <artyomchurin@gmail.com>
2022-04-06 05:23:25 +0300

    Добавьте файлы проекта.

---
+++ CommunicatingVessels/CommunicatingVesselsForm.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace CommunicatingVessels
+{
+    partial class CommunicatingVesselsForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.StartButton = new System.Windows.Forms.Button();
+            this.StopButton = new System.Windows.Forms.Button();
+            this.VTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // StartButton
+            // 
+            this.StartButton.Location = new System.Drawing.Point(12, 12);
+            this.StartButton.Name = "StartButton";
+            this.StartButton.Size = new System.Drawing.Size(75, 23);
+            this.StartButton.TabIndex = 0;
+            this.StartButton.Text = "Start";
+            this.StartButton.UseVisualStyleBackColor = true;
+            this.StartButton.Click += new System.EventHandler(this.StartButton_Click);
+            // 
+            // StopButton
+            // 
+            this.StopButton.Location = new System.Drawing.Point(12, 41);
+            this.StopButton.Name = "StopButton";
+            this.StopButton.Size = new System.Drawing.Size(75, 23);
+            this.StopButton.TabIndex = 1;
+            this.StopButton.Text = "Stop";
+            this.StopButton.UseVisualStyleBackColor = true;
+            this.StopButton.Click += new System.EventHandler(this.StopButton_Click);
+            // 
+            // VTextBox
+            // 
+            this.VTextBox.Location = new System.Drawing.Point(12, 94);
+            this.VTextBox.Name = "VTextBox";
+            this.VTextBox.Size = new System.Drawing.Size(74, 22);
+            this.VTextBox.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(25, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "V (л/c)";
+            // 
+            // CommunicatingVesselsForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.VTextBox);
+            this.Controls.Add(this.StopButton);
+            this.Controls.Add(this.StartButton);
+            this.Name = "CommunicatingVesselsForm";
+            this.Text = "Communicating Vessels";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.CommunicatingVesselsForm_Paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button StartButton;
+        private System.Windows.Forms.Button StopButton;
+        private System.Windows.Forms.TextBox VTextBox;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ CommunicatingVessels/CommunicatingVesselsForm.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommunicatingVessels
+{
+    public partial class CommunicatingVesselsForm : Form
+    {
+        private Graphics graph;
+        private Pen vesselsPen;
+        private Pen markPen;
+        private Pen waterPen;
+        private Timer timer;
+
+        private double width;
+        private double height;
+
+        private double k = 20;
+
+        private double waterLevel = 0;
+        private double dropWaterLevel = 25;
+
+        private static PointF generalCoords = new PointF(0, -10);
+        private static PointF bigVesCoords = new PointF(generalCoords.X - 20, generalCoords.Y);
+        private static PointF smallVesCoords = new PointF(generalCoords.X + 10, generalCoords.Y);
+        private static PointF intermediateVesCoords = new PointF(generalCoords.X, generalCoords.Y);
+        private static PointF globalWater = new PointF(bigVesCoords.X + 5, 25);
+
+        private void StartButton_Click(object sender, EventArgs e)
+        {
+            if (Regex.IsMatch(VTextBox.Text, @"^(([1-9][0-9])|([1-9]))$"))
+            {
+                StartButton.Enabled = false;
+                InitDropWaterTimer(1000 / 5 / Convert.ToInt32(VTextBox.Text));
+            }
+        }
+
+        private void StopButton_Click(object sender, EventArgs e)
+        {
+            timer.Stop();
+            dropWaterLevel = 25;
+            waterLevel = 0;
+            Invalidate();
+            StartButton.Enabled = true;
+        }
+
+        private void DropOne(Graphics graph)
+        {
+            globalWater = new PointF(bigVesCoords.X + 5, generalCoords.Y + (float)dropWaterLevel);
+
+            graph.DrawLine(waterPen, ScreenCoords(globalWater.X, globalWater.Y + 5),
+                ScreenCoords(globalWater.X + 5, globalWater.Y + 5));
+
+            graph.DrawLine(waterPen, ScreenCoords(globalWater.X, globalWater.Y),
+                ScreenCoords(globalWater.X + 5, globalWater.Y));
+
+            graph.DrawLine(waterPen, ScreenCoords(globalWater.X, globalWater.Y + 5),
+                ScreenCoords(globalWater.X, globalWater.Y));
+
+            graph.DrawLine(waterPen, ScreenCoords(globalWater.X + 5, globalWater.Y + 5),
+                ScreenCoords(globalWater.X + 5, globalWater.Y));
+        }
+
+        private void FillVessels(Graphics graph)
+        {
+            PaintBox(graph);
+            if (waterLevel < 5)
+            {
+                graph.DrawLine(waterPen, ScreenCoords(intermediateVesCoords.X - 5, intermediateVesCoords.Y + waterLevel),
+                    ScreenCoords(intermediateVesCoords.X + 10, intermediateVesCoords.Y + waterLevel));
+            }
+
+            graph.DrawLine(waterPen, ScreenCoords(smallVesCoords.X, smallVesCoords.Y + waterLevel),
+                ScreenCoords(smallVesCoords.X + 10, smallVesCoords.Y + waterLevel));
+
+            graph.DrawLine(waterPen, ScreenCoords(bigVesCoords.X, bigVesCoords.Y + waterLevel),
+                ScreenCoords(bigVesCoords.X + 15, bigVesCoords.Y + waterLevel));
+        }
+
+        private void PaintBox(Graphics graph)
+        {
+            for (int i = 0; i <= 15; i = i + 5)
+            {
+                graph.DrawLine(markPen, ScreenCoords(bigVesCoords.X + i, bigVesCoords.Y + 20),
+                    ScreenCoords(bigVesCoords.X + i, bigVesCoords.Y));
+            }
+
+            for (int i = 0; i <= 20; i = i + 5)
+            {
+                graph.DrawLine(markPen, ScreenCoords(bigVesCoords.X, bigVesCoords.Y + i),
+                    ScreenCoords(bigVesCoords.X + 15, bigVesCoords.Y + i));
+
+                graph.DrawLine(markPen, ScreenCoords(smallVesCoords.X + 10, smallVesCoords.Y + i),
+                    ScreenCoords(smallVesCoords.X, smallVesCoords.Y + i));
+            }
+
+            for (int i = 0; i <= 10; i = i + 5)
+            {
+                graph.DrawLine(markPen, ScreenCoords(smallVesCoords.X + i, smallVesCoords.Y + 20),
+                    ScreenCoords(smallVesCoords.X + i, smallVesCoords.Y));
+                graph.DrawLine(markPen, ScreenCoords(intermediateVesCoords.X + i, bigVesCoords.Y + 5),
+                    ScreenCoords(intermediateVesCoords.X + i, bigVesCoords.Y));
+            }
+
+            // Большой сосуд:
+            // Левая грань.
+            graph.DrawLine(vesselsPen, ScreenCoords(bigVesCoords.X, bigVesCoords.Y + 20),
+                ScreenCoords(bigVesCoords.X, bigVesCoords.Y));
+            // Правая грань.
+            graph.DrawLine(vesselsPen, ScreenCoords(bigVesCoords.X + 15, bigVesCoords.Y + 20),
+                ScreenCoords(bigVesCoords.X + 15, bigVesCoords.Y + 5));
+            //// Нижняя грань.
+            graph.DrawLine(vesselsPen, ScreenCoords(bigVesCoords.X, bigVesCoords.Y),
+                ScreenCoords(bigVesCoords.X + 15, bigVesCoords.Y));
+
+            //// Перемычка:
+            //// Верхняя грань.
+            graph.DrawLine(vesselsPen, ScreenCoords(intermediateVesCoords.X - 5, intermediateVesCoords.Y + 5),
+                ScreenCoords(intermediateVesCoords.X + 10, intermediateVesCoords.Y + 5));
+            //// Нижняя грань.
+            graph.DrawLine(vesselsPen, ScreenCoords(intermediateVesCoords.X - 5, intermediateVesCoords.Y),
+                ScreenCoords(intermediateVesCoords.X + 10, intermediateVesCoords.Y));
+
+            //// Малый сосуд:
+            //// Левая грань.
+            graph.DrawLine(vesselsPen, ScreenCoords(smallVesCoords.X, smallVesCoords.Y + 20),
+                ScreenCoords(smallVesCoords.X, smallVesCoords.Y + 5));
+            ////// Правая грань.
+            graph.DrawLine(vesselsPen, ScreenCoords(smallVesCoords.X + 10, smallVesCoords.Y + 20),
+                ScreenCoords(smallVesCoords.X + 10, smallVesCoords.Y));
+            ////// Нижняя грань.
+            graph.DrawLine(vesselsPen, ScreenCoords(smallVesCoords.X, smallVesCoords.Y),
+                ScreenCoords(smallVesCoords.X + 10, smallVesCoords.Y));
+        }
+
+        private PointF ScreenCoords(double x, double y)
+        {
+            return new PointF((float)(width / 2 + x * k), (float)(height / 2 - y * k));
+        }
+
+        public CommunicatingVesselsForm()
+        {
+            InitializeComponent();
+            graph = CreateGraphics();
+            graph.SmoothingMode = SmoothingMode.HighQuality;
+            vesselsPen = new Pen(Color.Black, 2);
+            markPen = new Pen(Color.Red, 1);
+            waterPen = new Pen(Color.Blue, 2);
+            width = ClientSize.Width;
+            height = ClientSize.Height;
+        }
+
+        private void InitDropWaterTimer(int interval)
+        {
+            timer = new Timer();
+            timer.Interval = interval;
+            timer.Tick += DropWaterTimer_Tick;
+            timer.Enabled = true;
+        }
+
+        private void DropWaterTimer_Tick(object sender, EventArgs e)
+        {
+            dropWaterLevel -= 5;
+            if (waterLevel == 20)
+            {
+                timer.Stop();
+            }
+            if (dropWaterLevel < waterLevel)
+            {
+                if (waterLevel < 5)
+                {
+                    waterLevel += 0.625;
+                }
+                else if (waterLevel < 20)
+                {
+                    waterLevel += 1;
+                }
+                dropWaterLevel = 25 + waterLevel;
+            }
+            Invalidate();
+        }
+
+        private void CommunicatingVesselsForm_Paint(object sender, PaintEventArgs e)
+        {
+            PaintBox(e.Graphics);
+            FillVessels(e.Graphics);
+            DropOne(e.Graphics);
+        }
+    }
+}
